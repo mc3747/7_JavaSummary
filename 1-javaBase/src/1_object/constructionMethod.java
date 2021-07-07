ff02943ef41cdec78bc00bb791babd9c7a42785b
小马 <mc3747@126.com>
2021-07-07 17:32:44 +0800

    更新方法

---
+++ 1-javaBase/src/1_object/constructionMethod.java
@@ -0,0 +1,34 @@
+/*
+* ✅构造方法✅
+*
+* */
+public class constructionMethod {
+    public static void main(String[] args) {
+        Person1 ming1 = new Person1();
+        Person1 ming2 = new Person1("kk");
+        Person1 ming3 = new Person1("pp", 12);
+        System.out.println(ming1);
+        System.out.println(ming2);
+        System.out.println(ming3);
+    }
+}
+class Person1 {
+    private String name;
+    private int age;
+    public Person1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public Person1(String name) {
+        this(name, 18); // 调用另一个构造方法Person(String, int)
+    }
+    public Person1() {
+        this("Unnamed"); // 调用另一个构造方法Person(String)
+    }
+//    重写打印的方法
+    @Override
+    public String toString() {
+        return "Person1(age:" + age + ",name:" + name + ")";
+    }
+
+}
---
+++ 1-javaBase/src/1_object/method.java
@@ -0,0 +1,46 @@
+/*
+ * ✅方法✅
+ *
+ * */
+public class method {
+    public static void main(String[] args) {
+        int age = 5;
+        String name = "a";
+        String[] fullName = new String[]{"mm","nn"};
+        Person ming = new Person();
+        ming.setAge(age);
+        ming.setName(name);
+        ming.setFullName(fullName);
+        System.out.println(ming.getAge() + ming.getName() + ming.getFullName());
+        age = 6;
+        name = "b";
+        fullName[0] = "kk";
+        System.out.println(ming.getAge() + ming.getName()+ ming.getFullName());
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+    private String[] fullName;
+// 传递基本类型
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+//  传递对象类型
+    public String getFullName() {
+        return this.fullName[0];
+    }
+    public  void  setFullName(String[] fullName){
+        this.fullName = fullName;
+    }
+}
